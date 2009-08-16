67aaf813065ce98f012e856ab178a2b221efc62a
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-08-16 13:20:42 -0500

    Added a client abstraction to work with for mario client connections

--- bin/agent.pl
+++ bin/agent.pl
@@ -3,61 +3,9 @@ use strict;
 use warnings;
 
 use FindBin;
-use Class::MOP;
-use POE qw(Component::Client::TCP Filter::Stream);
-use POE::Declarative;
-
 use lib "$FindBin::Bin/../lib";
 
-use AI::Mario::Observation;
+use AI::Mario::Client;
 
-my $agent;
-my $agent_class = $ARGV[0] || 'AI::Mario::Agent::Simple';
-
-my $need_reset  = 1;
-my $server_config 
-    = '-maxFPS on -ld 0 -lt 0 -mm 2 -ls 1 -tl 100 -pw off -vis on';
-
-on _start => run {
-    my $meta = Class::MOP::load_class($agent_class);
-    $agent = $meta->new_object;
-
-    get(KERNEL)->alias_set('agent');
-
-    POE::Component::Client::TCP->new(
-        Alias         => 'mario',
-        RemoteAddress => "localhost",
-        RemotePort    => 4242,
-        Filter        => 'POE::Filter::Stream',
-        Connected     => sub { 
-            post(agent => 'connected', $_[HEAP]{server}) 
-        },
-        ServerInput   => sub { 
-            post(agent => 'received', $_[HEAP]{server}, $_[ARG0]) 
-        },
-    );
-};
-
-sub tell_mario(@) {
-    get(ARG0)->put(join '', @_, "\r\n");
-}
-
-on connected => sub {
-    print "Connected @{[$agent->name]}\n";
-    tell_mario($agent->name);
-};
-
-on received => sub {
-    if ($need_reset) {
-        print "Sending reset... [$server_config]\n";
-        tell_mario("reset $server_config");
-        $need_reset = 0;
-    }
-
-    my $o = AI::Mario::Observation->new($_[ARG1]);
-    $agent->update($o);
-    tell_mario(join '', $agent->actions);
-};
-
-POE::Declarative->setup;
-POE::Kernel->run();
+my $client = AI::Mario::Client->new;
+$client->connect;
---
+++ lib/AI/Mario/Client.pm
@@ -0,0 +1,98 @@
+package AI::Mario::Client;
+use Moose;
+
+use POE qw(Component::Client::TCP Filter::Stream);
+use POE::Declarative;
+
+use AI::Mario::Observation;
+
+has agent => (
+    is        => 'rw',
+    does      => 'AI::Mario::Agent',
+    predicate => 'has_agent',
+);
+
+has agent_class => (
+    is        => 'ro',
+    isa       => 'Str',
+    required  => 1,
+    default   => 'AI::Mario::Agent::Simple',
+);
+
+has need_reset => (
+    is        => 'rw',
+    isa       => 'Bool',
+    required  => 1,
+    default   => 1,
+);
+
+has server_config => (
+    is        => 'rw',
+    isa       => 'Str',
+    required  => 1,
+    default   => '-maxFPS on -ld 0 -lt 0 -mm 2 -ls 1 -tl 100 -pw off -vis on',
+);
+
+sub setup_agent {
+    my $self = shift;
+
+    my $meta = Class::MOP::load_class($self->agent_class);
+    $self->agent($meta->new_object);
+}
+
+sub tell_mario(@) {
+    get(ARG0)->put(join '', @_, "\r\n");
+}
+
+on _start => run {
+    my $self = get(OBJECT);
+    $self->setup_agent;
+
+    get(KERNEL)->alias_set('agent');
+
+    POE::Component::Client::TCP->new(
+        Alias         => 'mario',
+        RemoteAddress => "localhost",
+        RemotePort    => 4242,
+        Filter        => 'POE::Filter::Stream',
+        Connected     => sub { 
+            post(agent => 'connected', $_[HEAP]{server}) 
+        },
+        ServerInput   => sub { 
+            post(agent => 'received', $_[HEAP]{server}, $_[ARG0]) 
+        },
+    );
+};
+
+on connected => sub {
+    my $self = get(OBJECT);
+    my $agent_name = $self->agent->name;
+
+    print "Connected $agent_name\n";
+    tell_mario($agent_name);
+};
+
+on received => sub {
+    my $self = get(OBJECT);
+    my $config = $self->server_config;
+
+    if ($self->need_reset) {
+        print "Sending reset... [$config]\n";
+        tell_mario("reset $config");
+        $self->need_reset(0);
+    }
+
+    my $o = AI::Mario::Observation->new($_[ARG1]);
+    my $agent = $self->agent;
+
+    $agent->update($o);
+    tell_mario(join '', $agent->actions);
+};
+
+sub connect {
+    my $self = shift;
+    POE::Declarative->setup($self);
+    POE::Kernel->run();
+}
+
+1;
